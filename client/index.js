3291b846d0b33162dc764ad936c0d4f4a81b0279
Tessa Lau <tlau@tlau.org>
2013-09-28 14:02:03 -0700

    Cleaned up teleop calls in prep for merging with joystick

--- client/index.js
+++ client/index.js
@@ -42,93 +42,86 @@ Template.video.myVideoSrc = function() {
 
 var teleop_cb = null;
 
-Template.teleop.events({
-  'mousedown #up': function() {
+var Robot = {
+  forward: function() {
+    $('#forward').addClass('alert');
     teleop_cb = window.setInterval(function() {
-      console.log('UP');
+      console.log('FORWARD');
       // Send a Twist message to the robot
       var msg = new ROSLIB.Message({
-        'linear': {
-          'x': 0,
-          'y': 0.5,
-          'z': 0
-        },
-        'angular': {
-          'x': 0,
-          'y': 0,
-          'z': 0
-        }
+        'linear': { 'x': 0, 'y': 0.5, 'z': 0 },
+        'angular': { 'x': 0, 'y': 0, 'z': 0 }
       });
-
-      CMDVEL.publish(msg);
-    }, 250);
-
-  },
-  'mouseup': function() {
-    console.log('End motion');
-    if (teleop_cb) {
-      window.clearInterval(teleop_cb);
-      teleop_cb = null;
-    }
-  },
-  'mousedown #down': function() {
-    teleop_cb = window.setInterval(function() {
-      console.log('DOWN');
-      // Send a Twist message to the robot
-      var msg = new ROSLIB.Message({
-        'linear': {
-          'x': 0,
-          'y': -0.5,
-          'z': 0
-        },
-        'angular': {
-          'x': 0,
-          'y': 0,
-          'z': 0
-        }
-      });
-
       CMDVEL.publish(msg);
     }, 250);
   },
-  'mousedown #left': function() {
+
+  back : function() {
+    $('#back').addClass('alert');
+    teleop_cb = window.setInterval(function() {
+      console.log('BACK');
+      // Send a Twist message to the robot
+      var msg = new ROSLIB.Message({
+        'linear': { 'x': 0, 'y': -0.5, 'z': 0 },
+        'angular': { 'x': 0, 'y': 0, 'z': 0 }
+      });
+      CMDVEL.publish(msg);
+    }, 250);
+  },
+
+  left: function() {
+    $('#left').addClass('alert');
     teleop_cb = window.setInterval(function() {
       console.log('LEFT');
       // Send a Twist message to the robot
       var msg = new ROSLIB.Message({
-        'linear': {
-          'x': 0,
-          'y': 0,
-          'z': 0
-        },
-        'angular': {
-          'x': -0.5,
-          'y': 0,
-          'z': 0
-        }
+        'linear': { 'x': 0, 'y': 0, 'z': 0 },
+        'angular': { 'x': -0.5, 'y': 0, 'z': 0 }
       });
-
       CMDVEL.publish(msg);
     }, 250);
   },
-  'mousedown #right': function() {
+
+  right: function() {
+    $('#right').addClass('alert');
     teleop_cb = window.setInterval(function() {
       console.log('RIGHT');
       // Send a Twist message to the robot
       var msg = new ROSLIB.Message({
-        'linear': {
-          'x': 0,
-          'y': 0,
-          'z': 0
-        },
-        'angular': {
-          'x': 0.5,
-          'y': 0,
-          'z': 0
-        }
+        'linear': { 'x': 0, 'y': 0, 'z': 0 },
+        'angular': { 'x': 0.5, 'y': 0, 'z': 0 }
       });
-
       CMDVEL.publish(msg);
     }, 250);
+  },
+
+  stop_moving: function() {
+    if (teleop_cb) {
+      window.clearInterval(teleop_cb);
+      teleop_cb = null;
+    }
+    $('#forward').removeClass('alert');
+    $('#back').removeClass('alert');
+    $('#left').removeClass('alert');
+    $('#right').removeClass('alert');
+  }
+};
+
+Template.teleop.events({
+  'mousedown #forward': function() {
+    Robot.forward();
+  },
+  'mouseup': function() {
+    console.log('End motion');
+    Robot.stop_moving();
+  },
+  'mousedown #back': function() {
+    Robot.back();
+  },
+  'mousedown #left': function() {
+    Robot.left();
+  },
+  'mousedown #right': function() {
+    Robot.right();
   }
 });
